ac2c34125372d716ab6f1537b9a17d59fa4c022f
samridhi-maharjan <samridhi.maharjan111@gmail.com>
2025-06-08 23:59:13 +0545

    JS variables, datatypes and operators

---
+++ DAY8/script.js
@@ -0,0 +1,31 @@
+//variables
+console.log("variables")
+ let x = 5;
+ let y=7;
+ let z = x+ y;
+ console.log("the result is:",z);
+
+ let a= "Samridhi";
+ let b= "Maharjan";
+ console.log(a +" " + b);
+
+ //operators
+ console.log("operators");
+ let num1=4, num2=7;
+ console.log("let num1=4, num2=7")
+ console.log("1.Addition");
+ console.log(num1+num2);
+ console.log("2.subtraction");  
+ console.log(num1-num2);
+ console.log("3.multiplication");  
+ console.log(num1*num2);
+ console.log("4.division");  
+ console.log(num1/num2);
+ console.log("5.comparision");  
+ if(num1>num2){
+    console.log("num1 is greater");
+ }
+ else{
+    console.log("num2 is greater");
+ }
+
